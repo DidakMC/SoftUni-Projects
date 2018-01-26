13470efa2b3ce3d6d952dd34650c480c4ff89a60
DidakMC <tonidid2003@gmail.com>
2018-01-26 12:36:38 +0200

    uploaded 2 and 3 solutions

---
+++ uprazneniq W3R 1/5/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Input the First Number:");
+            var a = Console.ReadLine();
+            Console.Write("Input the Second Number:");
+            var b = Console.ReadLine();
+            Console.WriteLine("After Swapping:");
+            Console.Write("First Number:");
+            Console.WriteLine(b);
+            Console.Write("Second Number:");
+            Console.WriteLine(a);
+
+        }
+    }
+}
